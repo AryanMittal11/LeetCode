d489b6fac942c71fd9b7a4ad54242e94c881c367
AryanMittal11 <164195062+AryanMittal11@users.noreply.github.com>
2024-10-04 01:09:04 +0530

    Time: 1 ms (98.32%), Space: 45 MB (5.68%) - LeetHub

--- 3347-distribute-elements-into-two-arrays-i/3347-distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/3347-distribute-elements-into-two-arrays-i.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
+        int[] arr1 = new int[nums.length];
+        int[] arr2 = new int[nums.length];
+        int x = 0; 
+        int y = 0;
+        arr1[0] = nums[0];
+        arr2[0] = nums[1];
         for (int i = 2; i < nums.length; i++){
-            if (arr1.get(arr1.size() - 1) > arr2.get(arr2.size() - 1)){
-                arr1.add(nums[i]);
+            if (arr1[x] > arr2[y]){
+                ++x;
+                arr1[x] = nums[i];
             } else {
-                arr2.add(nums[i]);
+                ++y;
+                arr2[y] = nums[i];
             }
         }
-        arr1.addAll(arr2);
-        int[] ans = new int[arr1.size()];
-        for (int i = 0; i < ans.length; i++){
-            ans[i] = arr1.get(i);
+        for (int i = 0; i <= y; i++){
+            arr1[x + 1] = arr2[i];
+            x++;
         }
-        return ans;
+        return arr1;
     }
 }
