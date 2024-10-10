95b77be47af75d9f0e32e514ded9f2c76e41122c
AryanMittal11 <164195062+AryanMittal11@users.noreply.github.com>
2024-10-11 01:19:42 +0530

    Time: 1 ms (100%), Space: 56.4 MB (48.07%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -3,14 +3,12 @@ class Solution {
         int j = -1;
         for (int i = 0; i < nums.length; i++){
             if (nums[i] == 1){
-                if (j == -1){
-                    j = i;
-                } else {
+                if (j != -1){
                     if (i - j - 1 < k){
                         return false;
-                    } 
-                    j = i;
+                    }
                 }
+                j = i;
             }
         }
         return true;
