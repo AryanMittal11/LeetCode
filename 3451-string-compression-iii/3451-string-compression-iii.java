1dd0eb187a43f6cf138f34ea8d196de22e033864
AryanMittal11 <164195062+AryanMittal11@users.noreply.github.com>
2024-11-04 19:24:58 +0530

    Time: 21 ms (54.24%), Space: 45.5 MB (91.88%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -6,15 +6,17 @@ class Solution {
         for (int i = 0; i < n; i++) {
             char ch = word.charAt(i);
             if (i < n - 1 && ch == word.charAt(i + 1)) {
+                count++;
                 if (count == 9) {
                     ans.append(count);
                     ans.append(ch);
                     count = 0;
                 }
-                count++;
             } else {
-                ans.append(count);
-                ans.append(ch);
+                if (count > 0) {
+                    ans.append(count);
+                    ans.append(ch);
+                }
                 count = 1;
             }
         }
