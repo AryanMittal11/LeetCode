2d2b184ad1f314ed229806aff5b99d8d75502648
AryanMittal11 <164195062+AryanMittal11@users.noreply.github.com>
2025-07-14 22:41:14 +0530

    Time: 1 ms (19.67%), Space: 42 MB (9.12%) - LeetHub

---
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                list.add(current.val);
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+                if (current.left != null && current.right != null) {
+                    if ((current.left.val == x || current.left.val == y) && (current.right.val == x || current.right.val == y)) {
+                    return false;
+                }
+                }
+            }
+            if (list.contains(x) && list.contains(y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
